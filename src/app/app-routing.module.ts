1d093ba2b102d66307a10d0d00ceb51ef6483f8d
gavindang2911 <hoaidang2999@gmail.com>
2022-04-11 00:24:06 -0700

    add angular routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AppComponent } from './app.component';
+import { BarChartComponent } from './bar-chart/bar-chart.component';
 import { HistoryComponent } from './history/history.component';
+import { TableComponent } from './table/table.component';
 
 const appRoutes: Routes = [
-  { path: 'history', component: HistoryComponent}
+  { path: 'history', component: HistoryComponent},
+  { path: 'chart', component: BarChartComponent},
+  { path: '', component: TableComponent}
 
 ];
 
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,28 +10,10 @@ import DEFAULT_FILTER, { Stats } from './stats.type';
 })
 export class AppComponent implements OnInit {
   title = 'Covid Tracker';
-  defaultFilter:Stats;
-  dataDefault = [];
-  d: any;
 
-  constructor(private ps: DataService, private fs: FilterService) {
-    this.defaultFilter = fs.filter;
+  constructor() {
   }
 
   ngOnInit() {
-    this.ps.getDataDefault().subscribe((data) => {
-      this.d = data;
-      this.dataDefault = this.d.summary.map((e) => e);
-      // console.log(this.DataArr);
-    });
-  }
-
-  updateStats(newStats: Stats) {
-    this.defaultFilter = newStats;
-    return this.ps.getDataDefault().subscribe((data) => {
-      this.d = data;
-      this.dataDefault = this.d.summary.map((e) => e);
-      console.log(this.dataDefault);
-    });
   }
 }
